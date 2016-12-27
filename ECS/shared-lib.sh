485b36d17b615cd6354dacc8a6a5ac26df8ff947
billot_t <thomas.billot@epitech.eu>
2016-12-27 14:46:36 +0100

    modified shared-lib.sh

--- ECS/shared-lib.sh
+++ ECS/shared-lib.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
 g++ -std=c++14 -shared -o mods/lib-MyMonster.so -fPIC srcs/MyMonster.cpp srcs/EntityManager.cpp srcs/SystemManager.cpp -Iincludes/
-# g++ -std=c++14 -shared -o dynamicLibs/lib-AnotherMonster.so -fPIC srcs/AnotherMonster.cpp -Iincludes/
