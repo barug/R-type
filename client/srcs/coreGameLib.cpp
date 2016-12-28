c7f62a276ffb4a5b9353b2e5cc479d1d8468d172
barthelemy gouby <barthelemy.gouby@gmail.com>
2016-12-28 19:11:15 +0100

    adding core lib file

---
+++ client/srcs/coreGameLib.cpp
@@ -0,0 +1,42 @@
+
+#include "SystemManager.hpp"
+#include "EntityManager.hpp"
+#include "PositionComponent.hpp"
+#include "PhysicComponent.hpp"
+#include "PhysicSystem.hpp"
+#include "GuiSystem.hpp"
+#include "SpriteComponent.hpp"
+#include "PlayerInputComponent.hpp"
+#include "PlayerInputSystem.hpp"
+
+void	loadGameLibData(EntityManager &e, SystemManager &s, MessageBus &m)
+{
+  e.addComponentType<PositionComponent>("PositionComponent");
+  e.addComponentType<PhysicComponent>("PhysicComponent");
+  e.addComponentType<PlayerInputComponent>("PlayerInputComponent");
+  e.addComponentType<SpriteComponent>("SpriteComponent");
+  s.addSystem(std::make_shared<GuiSystem>(e, m),
+	      "GuiSystem",
+	      {"SpriteComponent", "PositionComponent"},
+	      {});
+  s.addSystem(std::make_shared<PhysicSystem>(e, m),
+	      "PhysicSystem",
+	      {"PhysicComponent", "PositionComponent"},
+	      {});
+  s.addSystem(std::make_shared<PhysicSystem>(e, m),
+	      "PlayerInputSystem",
+	      {"PhysicComponent", "PlayerInputComponent"},
+	      {});
+  e.addEntityType("PlayerShip",
+		  e.getComponentMask("PositionComponent")
+		  | e.getComponentMask("PhysicComponent")
+		  | e.getComponentMask("PlayerInputComponent")
+		  | e.getComponentMask("SpriteComponent"));
+}
+
+typedef void (*loaderPtr)(EntityManager &, SystemManager &, MessageBus &);
+
+extern "C" loaderPtr returnLoader()
+{
+  return loadGameLibData;
+}
